9ce43aa42971b8f48d84706d8cc9adc064c8f56b
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-10-24 16:12:41 +0700

    Fix: integrate Framer Motion to Educations section

--- src/components/Educations.jsx
+++ src/components/Educations.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import { EDUCATIONS } from '../constants'
+import React from 'react';
+import { EDUCATIONS } from '../constants';
+import { motion } from 'framer-motion';
 
 const Educations = () => {
   return (
     <section className='py-8' id='educations'>
-      <h2 className='mb-4 text-3xl font-bold text-center'>Educations</h2>
+      <motion.h2 
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className='mb-4 text-3xl font-bold text-center'>Educations</motion.h2>
       {EDUCATIONS.map((education, index) => (
-        <div key={index} className='p-10 mb-6'>
+        <motion.div 
+          initial={{ opacity: 0, x: -20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: index * 0.5 }}
+          key={index} className='p-10 mb-6'>
           <h3 className='text-xl font-semibold'>{education.degree}</h3>
           <p className='text-lg'>{education.institution}</p>
           <p className='text-sm text-stone-300'>{education.duration}</p>
           <p className='mt-2'>{education.description}</p>
-        </div>
+        </motion.div>
       ))}
     </section>
   )
