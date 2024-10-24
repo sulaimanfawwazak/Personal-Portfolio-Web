79bce4c37ad168db731e59246b304bd0841872df
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-10-24 16:08:38 +0700

    Fix: integrate Framer Motion to Work Experiences  section

--- src/components/WorkExperiences.jsx
+++ src/components/WorkExperiences.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import { EXPERIENCES } from '../constants'
+import React from 'react';
+import { EXPERIENCES } from '../constants';
+import { motion } from 'framer-motion';
 
 const WorkExperiences = () => {
   return (
     <section className='pt-20' id='experiences'>
-      <h2 className='text-4xl font-semibold tracking-tighter text-center'>Work Experiences</h2>
-      <div className='p-10 space-y-8'>
+      {/* Title */}
+      <motion.h2 
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className='text-4xl font-semibold tracking-tighter text-center'
+        >
+          Work Experiences
+      </motion.h2>
+
+      {/* Experiences Start */}
+      <motion.div 
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className='p-10 space-y-8'>
         {EXPERIENCES.map((experience, index) => (
-          <div key={index} className='p-4 border rounded-xl border-stone-50/30 bg-white/10'>
+          <motion.div 
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: index * 2}}
+            key={index} className='p-4 border rounded-xl border-stone-50/30 bg-white/10'>
             <h3 className='text-2xl font-semibold'>{experience.title}</h3>
             <p className='text-xl'>{experience.company}</p>
             <p className='text-sm text-stone-300'>{experience.duration}</p>
             <p className='mt-2 text-base'>{experience.description}</p>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   )
 }
