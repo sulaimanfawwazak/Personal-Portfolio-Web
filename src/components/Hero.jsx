68e6c8ff309b21709f3e89bf8a9488e7a5eee885
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-10-29 14:37:19 +0700

    Fix: fix the responsive issue in the 'Hero' section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import Terminal from './Terminal';
 
 const Hero = () => {
   return (
-    <section className='flex flex-wrap items-center justify-center min-h-screen lg:px-5'>
+    <section className='flex flex-wrap items-center justify-center w-full min-h-screen md:px-5'>
 
       {/* Texts */}
       {/* Make full screen in mobile and 1/2 the width in desktop */}
@@ -17,17 +17,17 @@ const Hero = () => {
         className='w-full md:w-1/2'>
       
         {/* Name */}
-        <h2 className='my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]'>
+        <h2 className='my-8 p-2 text-4xl font-bold sm:text-5xl md:text-[5rem] lg:text-[7rem]'>
           {HERO.name}
         </h2>
       
         {/* Hello */}
-        <p className='p-2 text-3xl tracking-tighter lg:text-4xl'>
+        <p className='px-2 text-3xl tracking-tighter lg:text-4xl'>
           {HERO.greet}
         </p>
-        <p className='p-2 mb-8 text-xl'>
-          {HERO.description}
-        </p>
+        {/* <p className='p-2 mb-8 text-xl'> */}
+          {/* {HERO.description} */}
+        {/* </p> */}
       </motion.div>
 
       {/* Terminal */}
